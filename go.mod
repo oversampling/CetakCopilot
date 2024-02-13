7530e55ac659348088d63628d4a98aa46c74b09a
oversampling <chan1992241@gmail.com>
2024-02-14 00:49:32 +0800

    API call from client

--- go.mod
+++ go.mod
@@ -5,20 +5,58 @@ go 1.21.6
 require (
 	github.com/gofiber/fiber/v2 v2.52.0
 	github.com/gofiber/template/html/v2 v2.1.0
+	golang.org/x/oauth2 v0.16.0
+	google.golang.org/api v0.162.0
 )
 
 require (
+	cloud.google.com/go/compute v1.23.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	dario.cat/mergo v1.0.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/bep/godartsass v0.16.0 // indirect
+	github.com/bep/golibsass v1.1.0 // indirect
+	github.com/cli/safeexec v1.0.0 // indirect
+	github.com/cosmtrek/air v1.49.0 // indirect
+	github.com/creack/pty v1.1.18 // indirect
+	github.com/fatih/color v1.14.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gofiber/template v1.8.2 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
+	github.com/gohugoio/hugo v0.111.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	github.com/tdewolff/parse/v2 v2.6.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.47.0 // indirect
+	go.opentelemetry.io/otel v1.22.0 // indirect
+	go.opentelemetry.io/otel/metric v1.22.0 // indirect
+	go.opentelemetry.io/otel/trace v1.22.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240125205218-1f4bbc51befe // indirect
+	google.golang.org/grpc v1.61.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,41 +1,206 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
+)
+
+var (
+	art_card_350gsm = "art_card_350gsm"
+	box_board       = "box_board"
+	materials       = []string{art_card_350gsm, box_board}
+	categorySize    = []string{"A1+", "A1", "½A1 (RR)", "⅓A2++ (rr)", "A2++"}
 )
 
 type Quotation struct {
-	SizeCategory string "json:sizeCategory"
-	QuantityFrom int "json:quantityFrom"
-	QuantityTo int "json:quantityTo"
-	Material string "json:material"
-}	
+	SizeCategory string `json:"sizeCategory"`
+	Quantity     []int  `json:"quantity"`
+	Material     string `json:"material"`
+	NoOfColours  int    `json:"noOfColours"`
+	ReadiedSize  bool   `json:"readiedSize"`
+}
 
-func main()  {
+type Pricing struct {
+	Quantity    int      `json:"quantity"`
+	Price       []string `json:"price"`
+	PriceLabel  string   `json:"priceLabel"`
+	ReadiedSize bool     `json:"readiedSize"`
+	NoOfColours int      `json:"noOfColours"`
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func connectToGoogleSheet() (*sheets.Service, error) {
+	ctx := context.Background()
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	return srv, err
+}
+
+func load_env() error {
+	err := godotenv.Load()
+	return err
+}
+
+func getValueFromGoogleSheet(srv *sheets.Service, spreadsheetId string, range_ string) ([][]interface{}, error) {
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, range_).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
+	return resp.Values, err
+}
+
+func calcuateQuotation(quotation Quotation, value [][]interface{}) (Pricing, error) {
+	var search_str_material string
+	for i := 0; i < len(materials); i++ {
+		if materials[i] == quotation.Material {
+			search_str_material = quotation.Material
+			break
+		}
+	}
+	var colourSearchStr string
+	if quotation.NoOfColours <= 1 {
+		colourSearchStr = fmt.Sprintf("%d%s", quotation.NoOfColours, "colour")
+	} else {
+		colourSearchStr = fmt.Sprintf("%d%s", quotation.NoOfColours, "colours")
+	}
+	noOfQuantity := len(quotation.Quantity)
+	search_str_material = strings.Replace(search_str_material, "_", " ", -1)
+	prices := Pricing{
+		Quantity:    quotation.Quantity[0],
+		NoOfColours: quotation.NoOfColours,
+		ReadiedSize: quotation.ReadiedSize,
+		PriceLabel:  search_str_material,
+		Price:       []string{},
+	}
+	tempQuantitySearchIdx := 0
+	for _, row := range value {
+		if len(row) == 6 {
+			quantity_search := strconv.Itoa(quotation.Quantity[tempQuantitySearchIdx])
+			if row[2] == search_str_material && row[3] == quotation.SizeCategory && row[1] == colourSearchStr && row[4] == quantity_search {
+				prices.Price = append(prices.Price, fmt.Sprintf("%s pcs: %s printing ", row[4], row[5]))
+				tempQuantitySearchIdx++
+				if len(prices.Price) == noOfQuantity {
+					break
+				}
+			}
+		}
+	}
+	return prices, nil
+}
+
+func main() {
 	engine := html.New("./views", ".html")
-
+	err := load_env()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	spreadsheetId := os.Getenv("SPREADSHEET_ID")
+	srv, err := connectToGoogleSheet()
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
 	app := fiber.New(fiber.Config{
-        Views: engine,
-    })
-
-	app.Get("/", func(c *fiber.Ctx) error {
-        // Render index template
-        return c.Render("index", fiber.Map{
-            "Title": "Hello, World!",
-        })
-    })
-
-	app.Get("/getQuotation", func(c *fiber.Ctx) error {
-		// Connect to Google Sheets
-		quotation := new(Quotation)
-		if err := c.BodyParser(quotation); err != nil {
-            return err
-        }
-		return c.JSON(quotation)
+		Views: engine,
 	})
 
-    log.Fatal(app.Listen(":80"))
-	
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Render index template
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	app.Post("/getQuotation", func(c *fiber.Ctx) error {
+		quotation := new(Quotation)
+		if err := c.BodyParser(quotation); err != nil {
+			return err
+		}
+		log.Println(quotation)
+		log.Println(quotation.SizeCategory)
+		value, err := getValueFromGoogleSheet(srv, spreadsheetId, "Printing Raw!A1:F")
+		if err != nil {
+			return err
+		}
+		// log.Println(value[7401])
+		prices, _ := calcuateQuotation(*quotation, value)
+		return c.JSON(prices)
+	})
+
+	log.Fatal(app.Listen(":8000"))
+
 }
